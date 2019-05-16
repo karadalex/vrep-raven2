1d34138613de86a1d2a382b117f811c0039c1332
karadalex <karadalex@gmail.com>
2019-05-16 15:14:35 +0300

    move arm target dummies from UI for inverse kinematics

--- scripts/vrep-ui.lua
+++ scripts/vrep-ui.lua
@@ -10,11 +10,152 @@
 
 leftArmObjHandle = sim.getObjectHandle('base_link_L_visual')
 leftArmScriptHandle = sim.getScriptAssociatedWithObject(leftArmObjHandle)
+leftToolDummyTarget = sim.getObjectHandle("left_tool_target")
+
 rightArmObjHandle = sim.getObjectHandle('base_link_R_visual')
 rightArmScriptHandle = sim.getScriptAssociatedWithObject(rightArmObjHandle)
+rightToolDummyTarget = sim.getObjectHandle("right_tool_target")
+
 examplesObjHandle = sim.getObjectHandle('examples')
 examplesScriptHandle = sim.getScriptAssociatedWithObject(examplesObjHandle)
 
+
+function sysCall_init()
+    xml = [[
+    <ui closeable="true" on-close="closeEventHandler" resizable="true" size="400,450" title="Raven 2 UI">
+        <tabs>
+            <tab title="Forward Kinematics">
+                <label text="You must have IK groups disabled (Deselect from Inverse kinematics dialog)" />
+                <tabs>
+                    <tab title="Left arm">
+                        <group layout="vbox">
+                            <label text="shoulder [deg]" />
+                            <hslider id="1001" tick-position="below" tick-interval="10" minimum="-180" maximum="180" on-change="onLeftArmJointsChange"/>
+                            <label text="elbow [deg]" />
+                            <hslider id="1002" tick-position="below" tick-interval="10" minimum="-180" maximum="180" on-change="onLeftArmJointsChange"/>
+                            <label text="insertion [x100 micrometers]" />
+                            <hslider id="1003" tick-position="below" tick-interval="1000" minimum="-17000" maximum="7000" on-change="onLeftArmJointsChange"/>
+                            <label text="tool roll [deg]" />
+                            <hslider id="1004" tick-position="below" tick-interval="10" minimum="-180" maximum="180" on-change="onLeftArmJointsChange"/>
+                            <label text="wrist joint [deg]" />
+                            <hslider id="1005" tick-position="below" tick-interval="10" minimum="-180" maximum="180" on-change="onLeftArmJointsChange"/>
+                            <label text="grasper [deg]" />
+                            <hslider id="1006" tick-position="below" tick-interval="10" minimum="-180" maximum="180" on-change="onLeftArmJointsChange"/>
+                        </group>
+                    </tab>
+                    <tab title="Right arm">
+                        <group layout="vbox">
+                            <label text="shoulder [deg]" />
+                            <hslider id="2001" tick-position="below" tick-interval="10" minimum="-180" maximum="180" on-change="onRightArmJointsChange"/>
+                            <label text="elbow [deg]" />
+                            <hslider id="2002" tick-position="below" tick-interval="10" minimum="-180" maximum="180" on-change="onRightArmJointsChange"/>
+                            <label text="insertion [x100 micrometers]" />
+                            <hslider id="2003" tick-position="below" tick-interval="1000" minimum="-17000" maximum="7000" on-change="onRightArmJointsChange"/>
+                            <label text="tool roll [deg]" />
+                            <hslider id="2004" tick-position="below" tick-interval="10" minimum="-180" maximum="180" on-change="onRightArmJointsChange"/>
+                            <label text="wrist joint [deg]" />
+                            <hslider id="2005" tick-position="below" tick-interval="10" minimum="-180" maximum="180" on-change="onRightArmJointsChange"/>
+                            <label text="grasper [deg]" />
+                            <hslider id="2006" tick-position="below" tick-interval="10" minimum="-180" maximum="180" on-change="onRightArmJointsChange"/>
+                        </group>
+                    </tab>
+                </tabs>
+                <stretch />
+            </tab>
+
+
+            <tab title="Inverse Kinematics">
+                <label text="You must have IK groups enabled (Select from Inverse kinematics dialog)" />
+                <tabs>
+                    <tab title="Left arm">
+                        <group layout="vbox">
+                            <label text="X [x100 micrometers]" />
+                            <hslider id="3001" tick-position="below" tick-interval="200" minimum="-2500" maximum="2500" on-change="onLeftArmTargetPosChange"/>
+                            <label text="Y [x100 micrometers]" />
+                            <hslider id="3002" tick-position="below" tick-interval="200" minimum="-2500" maximum="2500" on-change="onLeftArmTargetPosChange"/>
+                            <label text="Z [x100 micrometers]" />
+                            <hslider id="3003" tick-position="below" tick-interval="200" minimum="0" maximum="5000" on-change="onLeftArmTargetPosChange"/>
+                            <label text="Yaw (alpha) [deg]" />
+                            <hslider id="3004" tick-position="below" tick-interval="10" minimum="-180" maximum="180" on-change="onLeftArmTargetPosChange"/>
+                            <label text="Pitch (beta) [deg]" />
+                            <hslider id="3005" tick-position="below" tick-interval="10" minimum="-180" maximum="180" on-change="onLeftArmTargetPosChange"/>
+                            <label text="Roll (gamma) [deg]" />
+                            <hslider id="3006" tick-position="below" tick-interval="10" minimum="-180" maximum="180" on-change="onLeftArmTargetPosChange"/>
+                        </group>
+                    </tab>
+                    <tab title="Right arm">
+                        <group layout="vbox">
+                            <label text="X [x100 micrometers]" />
+                            <hslider id="4001" tick-position="below" tick-interval="200" minimum="-2500" maximum="2500" on-change="onRightArmTargetPosChange"/>
+                            <label text="Y [x100 micrometers]" />
+                            <hslider id="4002" tick-position="below" tick-interval="200" minimum="-2500" maximum="2500" on-change="onRightArmTargetPosChange"/>
+                            <label text="Z [x100 micrometers]" />
+                            <hslider id="4003" tick-position="below" tick-interval="200" minimum="0" maximum="5000" on-change="onRightArmTargetPosChange"/>
+                            <label text="Yaw (alpha) [deg]" />
+                            <hslider id="4004" tick-position="below" tick-interval="10" minimum="-180" maximum="180" on-change="onRightArmTargetPosChange"/>
+                            <label text="Pitch (beta) [deg]" />
+                            <hslider id="4005" tick-position="below" tick-interval="10" minimum="-180" maximum="180" on-change="onRightArmTargetPosChange"/>
+                            <label text="Roll (gamma) [deg]" />
+                            <hslider id="4006" tick-position="below" tick-interval="10" minimum="-180" maximum="180" on-change="onRightArmTargetPosChange"/>
+                        </group>
+                    </tab>
+                </tabs>
+                <stretch />
+            </tab>
+
+
+            <tab title="Examples">
+                <group layout="vbox">
+                    <label text="Example 1" />
+                    <button text="Run" id="5001" on-click="runExample1" />
+                </group>
+                <stretch />
+            </tab>
+
+            <tab title="RemoteAPI">
+                <group layout="vbox">
+                        
+                </group>
+                <stretch />
+            </tab>
+
+            <tab title="ROS">
+                <group layout="vbox">
+                        
+                </group>
+                <stretch />
+            </tab>
+        </tabs>
+    </ui>
+    ]]
+	  
+    ui = simUI.create(xml)
+
+    -- Set initial values to UI sliders
+
+    -- x,y,z of left_tool_target dummy
+    leftToolDummyTargetPos = sim.getObjectPosition(leftToolDummyTarget, -1)
+    for i=1,3,1 do
+        simUI.setSliderValue(ui, 3000+i, leftToolDummyTargetPos[i])
+    end
+    -- alpha, beta, gamma of left_tool_target dummy
+    leftToolDummyTargetOrient = sim.getObjectOrientation(leftToolDummyTarget, -1)
+    for i=4,6,1 do
+        simUI.setSliderValue(ui, 3000+i, leftToolDummyTargetOrient[i])
+    end
+
+    -- x,y,z of right_tool_target dummy
+    rightToolDummyTargetPos = sim.getObjectPosition(rightToolDummyTarget, -1)
+    for i=1,3,1 do
+        simUI.setSliderValue(ui, 4000+i, rightToolDummyTargetPos[i])
+    end
+    -- alpha, beta, gamma of right_tool_target dummy
+    rightToolDummyTargetOrient = sim.getObjectOrientation(rightToolDummyTarget, -1)
+    for i=4,6,1 do
+        simUI.setSliderValue(ui, 4000+i, rightToolDummyTargetOrient[i])
+    end
+   
+end
 
 function sleep(time)
     local sec = tonumber(os.clock() + time);
@@ -54,141 +195,37 @@ function runExample1(ui, id)
 end
 
 function onLeftArmTargetPosChange(ui, id, newVal)
-    newPosition = {0,0,0,0,0,0}
-    for i = 3001,3006,1 do
-        newPosition[i-3000] = simUI.getSliderValue(ui, i) * math.pi/180
+    newPosition = {0,0,0}
+    newOrientation = {0,0,0}
+    for i = 3001,3003,1 do
+        newPosition[i-3000] = simUI.getSliderValue(ui, i) / 10000
     end
-    print(newPosition)
+    for i = 3004,3006,1 do
+        newOrientation[i-3003] = simUI.getSliderValue(ui, i) * math.pi/180
+    end
+    print(newPosition, newOrientation)
 
-    inInts,inFloats,inStrings,inBuffer = sim.callScriptFunction('commandLeftDummyTarget@base_link_L_visual', leftArmScriptHandle, {}, newPosition, {}, {})
-    print(inStrings[1])
+    sim.setObjectPosition(leftToolDummyTarget, -1, newPosition)
+    sim.setObjectOrientation(leftToolDummyTarget, -1, newOrientation)
+
     return
 end
 
 function onRightArmTargetPosChange(ui, id, newVal)
-    newPosition = {0,0,0,0,0,0}
-    for i = 4001,4006,1 do
-        newPosition[i-4000] = simUI.getSliderValue(ui, i) * math.pi/180
+    newPosition = {0,0,0}
+    newOrientation = {0,0,0}
+    for i = 4001,4003,1 do
+        newPosition[i-4000] = simUI.getSliderValue(ui, i) / 10000
     end
-    print(newPosition)
+    for i = 4004,4006,1 do
+        newOrientation[i-4003] = simUI.getSliderValue(ui, i) * math.pi/180
+    end
+    print(newPosition, newOrientation)
 
-    inInts,inFloats,inStrings,inBuffer = sim.callScriptFunction('commandRightDummyTarget@base_link_R_visual', rightArmScriptHandle, {}, newPosition, {}, {})
-    print(inStrings[1])
+    sim.setObjectPosition(rightToolDummyTarget, -1, newPosition)
+    sim.setObjectOrientation(rightToolDummyTarget, -1, newOrientation)
+
     return
-end
-
-
-function sysCall_init()
-    xml = [[
-    <ui closeable="true" on-close="closeEventHandler" resizable="true" size="400,450" title="Raven 2 UI">
-        <tabs>
-            <tab title="Forward Kinematics">
-                <label text="You must have IK groups disabled (Deselect from Inverse kinematics dialog)" />
-                <tabs>
-                    <tab title="Left arm">
-                        <group layout="vbox">
-                            <label text="shoulder" />
-                            <hslider id="1001" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onLeftArmJointsChange"/>
-                            <label text="elbow" />
-                            <hslider id="1002" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onLeftArmJointsChange"/>
-                            <label text="insertion" />
-                            <hslider id="1003" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onLeftArmJointsChange"/>
-                            <label text="tool roll" />
-                            <hslider id="1004" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onLeftArmJointsChange"/>
-                            <label text="wrist joint" />
-                            <hslider id="1005" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onLeftArmJointsChange"/>
-                            <label text="grasper" />
-                            <hslider id="1006" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onLeftArmJointsChange"/>
-                        </group>
-                    </tab>
-                    <tab title="Right arm">
-                        <group layout="vbox">
-                            <label text="shoulder" />
-                            <hslider id="2001" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onRightArmJointsChange"/>
-                            <label text="elbow" />
-                            <hslider id="2002" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onRightArmJointsChange"/>
-                            <label text="insertion" />
-                            <hslider id="2003" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onRightArmJointsChange"/>
-                            <label text="tool roll" />
-                            <hslider id="2004" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onRightArmJointsChange"/>
-                            <label text="wrist joint" />
-                            <hslider id="2005" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onRightArmJointsChange"/>
-                            <label text="grasper" />
-                            <hslider id="2006" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onRightArmJointsChange"/>
-                        </group>
-                    </tab>
-                </tabs>
-                <stretch />
-            </tab>
-
-
-            <tab title="Inverse Kinematics">
-                <label text="You must have IK groups enabled (Select from Inverse kinematics dialog)" />
-                <tabs>
-                    <tab title="Left arm">
-                        <group layout="vbox">
-                            <label text="X" />
-                            <hslider id="3001" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onLeftArmTargetPosChange"/>
-                            <label text="Y" />
-                            <hslider id="3002" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onLeftArmTargetPosChange"/>
-                            <label text="Z" />
-                            <hslider id="3003" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onLeftArmTargetPosChange"/>
-                            <label text="Yaw (alpha)" />
-                            <hslider id="3004" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onLeftArmTargetPosChange"/>
-                            <label text="Pitch (beta)" />
-                            <hslider id="3005" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onLeftArmTargetPosChange"/>
-                            <label text="Roll (gamma)" />
-                            <hslider id="3006" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onLeftArmTargetPosChange"/>
-                        </group>
-                    </tab>
-                    <tab title="Right arm">
-                        <group layout="vbox">
-                            <label text="X" />
-                            <hslider id="4001" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onRightArmTargetPosChange"/>
-                            <label text="Y" />
-                            <hslider id="4002" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onRightArmTargetPosChange"/>
-                            <label text="Z" />
-                            <hslider id="4003" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onRightArmTargetPosChange"/>
-                            <label text="Yaw (alpha)" />
-                            <hslider id="4004" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onRightArmTargetPosChange"/>
-                            <label text="Pitch (beta)" />
-                            <hslider id="4005" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onRightArmTargetPosChange"/>
-                            <label text="Roll (gamma)" />
-                            <hslider id="4006" tick-position="below" tick-interval="10" minimum="0" maximum="90" on-change="onRightArmTargetPosChange"/>
-                        </group>
-                    </tab>
-                </tabs>
-                <stretch />
-            </tab>
-
-
-            <tab title="Examples">
-                <group layout="vbox">
-                    <label text="Example 1" />
-                    <button text="Run" id="5001" on-click="runExample1" />
-                </group>
-                <stretch />
-            </tab>
-
-            <tab title="RemoteAPI">
-                <group layout="vbox">
-                        
-                </group>
-                <stretch />
-            </tab>
-
-            <tab title="ROS">
-                <group layout="vbox">
-                        
-                </group>
-                <stretch />
-            </tab>
-        </tabs>
-    </ui>
-    ]]
-	  
-   ui = simUI.create(xml)
-   
 end
 
 function sysCall_actuation()
